650c3b475d4334851a00a9b8202080ef8cfcba39
Jeff Hwang <wodud6359@gmail.com>
2024-06-18 13:42:14 +1200

    Court

--- booking.football/src/components/Courts/Courts.js
+++ booking.football/src/components/Courts/Courts.js
@@ -4,23 +4,53 @@ import Modal from 'react-bootstrap/Modal';
 import "./Court.css";
 
 const Courts = ({ court }) => {
-    // const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
 
-    // const handleClose = () => setShow(false);
-    // const handleShow = () => setShow(true);
+    const handleClose = () => {
+        setShow(false)
+    };
+    const handleShow = () => {
+        setShow(true)
+    };
 
     return (
-        <div className="row">
+        <div className="row bs">
 
             <div className="col-md-4">
-                <img src={court.imgURLs[0]} className="smallimg" />
+                <img src={court?.imgURLs[0]} className="small-img" />
             </div>
 
             <div className="col-md-7">
+                <h1>{court.name}</h1>
+                <p>Price :  {court.price}</p>
+                <p>Max Players :  {court.maxPlayers}</p>
+                <p>Location :  {court.location}</p>
 
+                <div className="view-details">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
             </div>
 
-            <h1>{court.name}</h1>
+
+            <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
         </div>
     );
 }
