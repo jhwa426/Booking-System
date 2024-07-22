ad3d4337ec99e11d875654639d2c69a876bde270
Jeff Hwang <wodud6359@gmail.com>
2024-07-23 10:18:15 +1200

    Backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: ['https://booking-football.onrender.com'],
+    origin: ['http://localhost:3000', 'https://booking-football.onrender.com'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
