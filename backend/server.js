f927c99cac242b78bdbd9794d9f1e7a02071fff7
Jeff Hwang <wodud6359@gmail.com>
2024-07-22 12:19:37 +1200

    Backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-    origin: ['https://booking-football.netlify.app'],
+    origin: ['https://booking-system-frontend.onrender.com'],
     methods: ['GET,POST'],
     credentials: true
 }));
